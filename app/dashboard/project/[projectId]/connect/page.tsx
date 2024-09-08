454de3f5b92eda7aa340d8bbbf8513069a6a1ae0
Carter Adams <cladams7905@gmail.com>
2024-09-08 16:43:32 +0800

    Fix type error

--- app/dashboard/project/[projectId]/connect/page.tsx
+++ app/dashboard/project/[projectId]/connect/page.tsx
@@ -17,7 +17,6 @@ export default async function ConnectPage() {
   return (
     <div className="flex flex-col items-center justify-center gap-3 bg-gradient-to-tr from-primary/50 to-violet-100 h-screen-minus-navbar w-full lg:px-12">
       <IntegrationBoard
-        user={userData.user}
         fetchedActiveProject={activeProject}
         fetchedIntegrations={integrations}
       />
