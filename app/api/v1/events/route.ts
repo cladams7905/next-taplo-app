f9e1d9dd1bb72079a1698c0643731fb26070d6c2
Carter Adams <cladams7905@gmail.com>
2024-09-29 11:40:17 +0800

    Adding cors

--- app/api/v1/events/route.ts
+++ app/api/v1/events/route.ts
@@ -128,3 +128,19 @@ async function getChargesFromStripe(stripe: Stripe, timeToFilter: number) {
 
   return charges;
 }
+
+export async function OPTIONS(request: Request) {
+  const allowedOrigin = request.headers.get("origin");
+  const response = new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": allowedOrigin || "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+      "Access-Control-Max-Age": "86400",
+    },
+  });
+
+  return response;
+}
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,48 @@
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { updateSession } from "@/supabase/middleware";
+import Cors from "cors";
+
+// Initialize the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD", "OPTIONS", "POST", "PUT", "PATCH", "DELETE"],
+  origin: "https://www.taplo.io",
+  credentials: true,
+  allowedHeaders: [
+    "X-CSRF-Token",
+    "X-Requested-With",
+    "Accept",
+    "Accept-Version",
+    "Content-Length",
+    "Content-MD5",
+    "Content-Type",
+    "Date",
+    "X-Api-Version",
+  ],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+function runMiddleware(req: NextRequest, res: NextResponse, fn: Function) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  // Run the CORS middleware
+  await runMiddleware(request, response, cors);
+
+  // Skip middleware for preflight requests
+  if (request.method === "OPTIONS") {
+    return response;
+  }
+
   return await updateSession(request);
 }
 
@@ -14,9 +55,8 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico).*)",
     "/(auth)/:path*",
     "/dashboard/:path*",
-    "/api/:path*",
   ],
 };
