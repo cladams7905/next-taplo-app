347b3d61fb9c34f64d308274e66b2fa955747ea6
Carter Adams <cladams7905@gmail.com>
2024-09-29 10:51:59 +0800

    Adding cors

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // Apply headers to all routes
-        source: "/(.*)",
+        // matching all API routes
+        source: "/api/:path*",
         headers: [
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
