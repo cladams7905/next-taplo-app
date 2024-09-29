8aa6a323f9424ef0331b923f37209252d9f4018c
Carter Adams <cladams7905@gmail.com>
2024-09-29 11:50:50 +0800

    Fixing cors

--- middleware.ts
+++ middleware.ts
@@ -1,48 +1,7 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
 import { updateSession } from "@/supabase/middleware";
-import Cors from "cors";
-
-// Initialize the cors middleware
-const cors = Cors({
-  methods: ["GET", "HEAD", "OPTIONS", "POST", "PUT", "PATCH", "DELETE"],
-  origin: "https://www.taplo.io",
-  credentials: true,
-  allowedHeaders: [
-    "X-CSRF-Token",
-    "X-Requested-With",
-    "Accept",
-    "Accept-Version",
-    "Content-Length",
-    "Content-MD5",
-    "Content-Type",
-    "Date",
-    "X-Api-Version",
-  ],
-});
-
-// Helper method to wait for a middleware to execute before continuing
-function runMiddleware(req: NextRequest, res: NextResponse, fn: Function) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-}
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-
-  // Run the CORS middleware
-  await runMiddleware(request, response, cors);
-
-  // Skip middleware for preflight requests
-  if (request.method === "OPTIONS") {
-    return response;
-  }
-
   return await updateSession(request);
 }
 
@@ -53,9 +12,10 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - api (API routes)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    "/((?!_next/static|_next/image|favicon.ico|api).*)",
     "/(auth)/:path*",
     "/dashboard/:path*",
   ],
