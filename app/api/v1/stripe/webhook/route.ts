035a425ab0cb7528d3f69ab393690d43d298a49e
Carter Adams <cladams7905@gmail.com>
2025-01-08 08:25:36 -0700

    Add eror message in webhook

--- app/api/v1/stripe/webhook/route.ts
+++ app/api/v1/stripe/webhook/route.ts
@@ -70,6 +70,7 @@ export async function POST(request: NextRequest) {
     } catch (error: any) {
       return NextResponse.json({
         message: "Webhook handler failed. View your nextjs function logs.",
+        error: error,
         status: 400,
       });
     }
